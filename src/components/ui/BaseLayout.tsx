9ce19c188e9ebabe7eb92eff27d0a402bdcf518e
tomzdon <tomekk.wlodarczyk@gmail.com>
2025-05-15 09:15:30 +0000

    Restored to '79036a35ce4506f6353597f63e636ab8f2655ec4'
    Replit-Restored-To: 79036a35ce4506f6353597f63e636ab8f2655ec4

--- src/components/ui/BaseLayout.tsx
+++ src/components/ui/BaseLayout.tsx
@@ -11,16 +11,14 @@ interface BaseLayoutProps {
 
 export const BaseLayout: React.FC<BaseLayoutProps> = ({ children }) => {
   return (
-    <div className="flex flex-col h-screen bg-neutral-darkest max-w-[660px] m-auto relative">
+    <div className="flex flex-col h-screen bg-neutral-darkest max-w-[660px] m-auto">
       {/* Header */}
-      <header className="bg-neutral-darkest p-4 fixed top-0 left-0 right-0 z-50">
-        <div className="max-w-[660px] mx-auto">
-          <img src="/betPawa.svg" alt="betPawa" />
-        </div>
+      <header className="bg-neutral-darkest p-4 ">
+        <img src="/betPawa.svg" alt="betPawa" />
       </header>
 
       {/* Main Content */}
-      <main className="flex-1 bg-white relative mt-[72px]">{children}</main>
+      <main className="flex-1 bg-white relative">{children}</main>
 
       {/* Bottom Banner */}
 
