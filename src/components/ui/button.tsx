200c06e21781e8e7c3237be9fabf05cb95918615
tomasz-wlodarczyk-pawatech <tomasz.wlodarczyk@pawatech.com>
2025-05-25 16:16:35 +0200

    Buttons

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,19 +5,19 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap transition-all disabled:pointer-events-none disabled:opacity-50 outline-none focus-visible:ring-[3px] focus-visible:ring-ring/50 text-[14px] leading-[18px] font-bold uppercase font-roboto gap-2 [&_svg]:pointer-events-none [&_svg:not([class*=\'size-\'])]:size-4 shrink-0 [&_svg]:shrink-0',
+  'flex items-center justify-center whitespace-nowrap transition-all disabled:pointer-events-none disabled:opacity-50 outline-none focus-visible:ring-[3px] focus-visible:ring-ring/50 button-bold gap-2 [&_svg]:pointer-events-none [&_svg:not([class*=\'size-\'])]:size-4 shrink-0 [&_svg]:shrink-0',
   {
     variants: {
       variant: {
-        primary: 'bg-[#b6e40d] text-neutral-darker',
-        secondary: 'bg-neutral-darkest text-white ',
-        tertiary: 'bg-neutral-darkest text-white border border-white',
-        outline: 'bg-transparent text-neutral-darkest border border-neutral-dark',
-        text: 'bg-transparent text-neutral-darkest font-normal gap-1 hover:text-neutral-medium',
+        primary: 'bg-primary-500 text-brand-foreground-light',
+        secondary: 'bg-neutral-800 dark:bg-white text-white dark:text-brand-foreground-light',
+        tertiary: 'bg-transparent text-brand-foreground-light border border-text-brand-foreground-light dark:border-white dark:text-white',
+        outline: 'bg-transparent text-brand-foreground-light border border-neutral-dark',
+        text: 'bg-transparent text-brand-foreground-light font-normal gap-1 hover:text-neutral-medium dark:text-white',
 
       },
       size: {
-        medium: 'px-[8px] py-[7px]',
+        medium: 'py-[8px] px-[16px]',
         large: 'px-[20px] py-[10px]',
         icon: 'size-9',
       },
@@ -32,7 +32,8 @@ const buttonVariants = cva(
 interface ButtonProps extends React.ComponentProps<'button'>, VariantProps<typeof buttonVariants> {
   asChild?: boolean;
   title?: string;
-  icon?: React.ReactNode;
+  Icon?: React.ReactNode;
+  iconName?: string;
   fullwidth?: boolean;
   titleClassName?: string;
 }
@@ -40,10 +41,11 @@ interface ButtonProps extends React.ComponentProps<'button'>, VariantProps<typeo
 function Button({
                   className,
                   variant,
-                  size,
+                  size = 'medium',
                   asChild = false,
                   title,
-                  icon,
+                  Icon,
+                  iconName,
                   fullwidth,
                   titleClassName,
                   ...props
@@ -59,7 +61,8 @@ function Button({
       )}
       {...props}
     >
-      {icon && <span>{icon}</span>}
+      {Icon && Icon}
+      {iconName && <i className={`fa-kit ${iconName}`}></i>}
       {title && <span className={cn(titleClassName)}>{title}</span>}
     </Comp>
   );
